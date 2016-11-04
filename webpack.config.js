b56a12c3116f5dae7b3d5b7b9c8020812b4ad104
Zak B. Elep <zakame@zakame.net>
2016-11-04 18:04:38 +0800

    Add new webpack loaders and jquery stuff
    Retain using jQuery for now, deal with moving off it later.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,39 @@
 module.exports = {
   devServer: {
     compress: true,
-    contentBase: './public'
+    contentBase: './public',
+    host: '0.0.0.0',
+    port: '8080',
+    proxy: {
+      '/api': {
+        target: 'http://backend:4711'
+      }
+    }
   },
   entry: {
+    library: './src/library/js/app.js',
     todos: './src/todos/js/app.js'
   },
   output: {
     path: __dirname + '/public',
+    publicPath: '/',
     filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(jpe?g|png|gif|)$/i,
+        loader: 'file-loader'
+      },
+    ]
+  },
+  resolve: {
+    alias: {
+      'jquery-dateformat': 'jquery-dateformat/dist/jquery-dateFormat.js'
+    }
   }
 };
