508940a2de9f5d8b500f53cff9805770734c6874
Zak B. Elep <zakame@zakame.net>
2016-11-05 18:15:15 +0800

    Move todos globals
    Not needed in the library.

--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,2 @@
 _ = require('underscore');
 Backbone = require('backbone');
-
-ENTER_KEY = 13;
-TodoFilter = '';
--- src/todos/js/app.js
+++ src/todos/js/app.js
@@ -1,5 +1,8 @@
 require('../../globals');
 
+ENTER_KEY = 13;
+TodoFilter = '';
+
 var AppView = require('./views/app');
 var Router = require('./routers/router');
 
